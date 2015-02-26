305b7cd0f966c5981868a28c78766b7ed94b9e11
Ahmad Saeed <asaeed@gmail.com>
2015-02-26 18:00:32 -0500

    added bootstrap-sass, swig precompiled and runtime templates, browser sync 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var merge = require('merge-stream');
 var buffer = require('gulp-buffer');
 var swig = require('gulp-swig');
 var browserSync = require('browser-sync');
@@ -24,12 +25,21 @@ gulp.task('clean-css', function(done) {
     del(['build/css'], done);
 });
 
+gulp.task('clean-vendor', function(done) {
+    del(['build/vendor'], done);
+});
+
 // create html from partials using swig
 gulp.task('html', ['clean-html'], function () {
-    return gulp.src('src/partials/*.html')
+    var pages = gulp.src('src/html/*.html')
         .pipe(buffer())
         .pipe(swig({ defaults: { cache: false } }))
         .pipe(gulp.dest('build'));
+
+    var partials = gulp.src('src/html/partials/**/*')
+        .pipe(gulp.dest('build/partials'));
+
+    return merge(pages, partials);
 });
 
 // lint
@@ -53,9 +63,25 @@ gulp.task('js', ['clean-js', 'lint'], function() {
 
 // sass
 gulp.task('sass', ['clean-css'], function() {
-    return gulp.src('src/scss/*.scss')
+    return gulp.src('src/scss/style.scss')
         .pipe(sass())
         .pipe(gulp.dest('./build/css'));
+});
+
+// vendor
+gulp.task('vendor', ['clean-vendor'], function() {
+    var s = 'bower_components/';
+    var d = './build/vendor/';
+    var bootstrap = gulp.src(s + 'bootstrap/dist/**/*')
+        .pipe(gulp.dest(d + 'bootstrap'));
+
+    var bootstrapSass = gulp.src('src/scss/bootstrap.scss')
+        //.pipe(sass())
+        .pipe(sass({ includePaths : [s + 'bootstrap-sass/assets/stylesheets'], errLogToConsole: true }))
+        .pipe(gulp.dest(d + 'bootstrap-sass/css'));
+
+    // if copying other libs, use merge to return merged stream
+    return merge(bootstrap, bootstrapSass);
 });
 
 //browser-sync will reload when compiled versions change
@@ -68,9 +94,9 @@ gulp.task('browser-sync', function() {
 });
 
 // default task when you run 'gulp'
-gulp.task('default', ['clean-html', 'clean-js', 'clean-css', 'html', 'lint', 'js', 'sass', 'browser-sync'], function() {
+gulp.task('default', ['clean-html', 'clean-js', 'clean-css', 'clean-vendor', 'html', 'lint', 'js', 'sass', 'vendor', 'browser-sync'], function() {
     // watch and recompile
-    gulp.watch('src/partials/*.html', ['clean-html', 'html', browserSync.reload]);
+    gulp.watch(['src/html/*.html', 'src/html/layouts/*.html', 'src/html/partials/*.html'], ['clean-html', 'html', browserSync.reload]);
     gulp.watch('src/js/*.js', ['clean-js', 'lint', 'js', browserSync.reload]);
     gulp.watch('src/scss/*.scss', ['clean-css', 'sass', browserSync.reload]);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 
-console.log('begins!');
+console.log('begin app.js');
+
+var swig = require('gulp-swig/node_modules/swig');
+
+// example of loading template in runtime
+$.get('partials/tile.html', function(template) {
+	var tile = swig.render(template, { locals: { name: 'Bob Loblaw' }});
+	console.log(tile);
+	$('#runtime-template').html(tile);
+});
+
+
